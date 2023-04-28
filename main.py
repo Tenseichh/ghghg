fb6fc5cf4024f676c5c122c3f3a01cb0a3fa0194
Tenseichh <124991502+Tenseichh@users.noreply.github.com>
2023-04-28 17:19:45 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import telebot
+photo_urls = ['https://imageup.ru/img40/4315132/img_20230427_205051.jpg',
+              "https://imageup.ru/img282/4315174/img-20230402-wa0016.jpg" ,
+              "https://imageup.ru/img204/4315151/img_20230422_084327.jpg",
+              "https://imageup.ru/img166/4315173/img-20230329-wa0149.jpg",
+              "https://imageup.ru/img229/4315167/img-20230329-wa0159.jpg",
+              "https://imageup.ru/img197/4315165/img-20230222-wa0058.jpg",
+              "https://imageup.ru/img99/4315163/img_20230411_154704.jpg",
+              "https://imageup.ru/img53/4315157/img_20230411_155350_3.jpg"]
+API = "6076822748:AAHayRfM0kq9QgUHcjrCUk7lXa03MgG5NOE"
+bot = telebot.TeleBot(API)
